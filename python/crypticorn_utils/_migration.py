da53c6e63d985c8d417b34068975d03e9dab3256
timonrieger <mail@timonrieger.de>
2025-07-18 00:07:12 +0200

    add test for exceptions

--- python/crypticorn_utils/_migration.py
+++ python/crypticorn_utils/_migration.py
@@ -1,5 +1,6 @@
 # This file is used to check if the crypticorn version is greater than 2.18
 # This is to be compatible with this new utils library
+# otherwise prometheus reqisters metrics twice, resulting in an exception
 
 import importlib.metadata
 from importlib.metadata import PackageNotFoundError
@@ -9,4 +10,4 @@ try:
     parts = crypticorn_version.split(".")
     has_migrated = parts[0] >= "2" and parts[1] > "18"
 except PackageNotFoundError:
-    has_migrated = False
+    has_migrated = True
